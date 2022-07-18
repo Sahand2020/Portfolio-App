b214b0e048e58703258575e93bf1efaf2e55179d
Sahand <76976809+Sahand2020@users.noreply.github.com>
2022-07-18 20:36:07 +0430

    footer link : target=_blank

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,12 @@ import "./Footer.css";
 
 const Footer = () => (
     <footer className="footer">
-        <a href="https://github.com/Sahand2020" className="link footer_link">
+        <a
+            href="https://github.com/Sahand2020"
+            target="_blank"
+            rel="noreferrer"
+            className="link footer_link"
+        >
             Created By Sahand Torabnejad
         </a>
     </footer>
