a66d72e5fbff15d842e42bee5636e71269968720
Sahand <76976809+Sahand2020@users.noreply.github.com>
2022-07-18 20:35:12 +0430

    footer link changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import "./Footer.css";
 
 const Footer = () => (
     <footer className="footer">
-        <a href="https://github.com" className="link footer_link">
+        <a href="https://github.com/Sahand2020" className="link footer_link">
             Created By Sahand Torabnejad
         </a>
     </footer>
